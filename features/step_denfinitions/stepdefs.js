a43ad0520f04a40f9f0590e78a385159d21fb79a
= <=>
2021-11-19 20:15:23 +0600

    BDD implemented

--- features/step_denfinitions/stepdefs.js
+++ features/step_denfinitions/stepdefs.js
@@ -3,24 +3,19 @@ const { Given, When, Then } = require('cucumber');
 const loginPage = require('../../page-objects/pages/LoginPage');
 const URL = `https://opencart.abstracta.us/index.php?route=account/login`
 
+
 Given('I am on the login page', async function () {
-    await testController.navigateTo(URL); 
+
+    await testController.navigateTo(URL);
+    //const getPageUrl = ClientFunction(()=> window.location.href);
+    //await testController.expect(getPageUrl()).contains("login");
+
   });
 
-// Given('I am on the login page', async function () {
-
-//     await testController.navigateTo(URL);
-//     const getPageUrl = ClientFunction(()=> window.location.href);
-//     await testController.expect(getPageUrl()).contains("login");
-
-//   });
-
-  When('I type <username> and <password>', async function () {
-
+  When('I type {string} and {string}', async function (username, password) {
     await testController.typeText(loginPage.LoginPage.emailField(),username);
-    await testController.typeText(loginPage.LoginPage.passwordField(),password);
-
-  });
+    await testController.typeText(loginPage.LoginPage.passwordField(),password);  
+ });
 
   When('I click on the login button', async function () {
 
